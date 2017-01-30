01f86b2ab80a46d282660df5c9aea0c707282135
Ryan <ryanpsuresh@gmail.com>
2017-01-30 17:57:47 -0600

    Set up initial server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
 const express = require('express');
 
 const app = express();
+
+app.use(express.static(`${__dirname}./../client/public`));
+
 app.listen(8000);
+console.log('listening on Port 8000');
