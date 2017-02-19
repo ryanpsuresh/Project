6fdf401beee66b5197df8dfa6beb32eae8c4852c
Ryan <ryanpsuresh@gmail.com>
2017-02-18 20:27:53 -0600

    create initial landingpage

---
+++ client/app/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const styles = {
+  
+}
+
+const LandingPage = () => (
+  <h1> LandingPage </h1>
+)
+
+export default LandingPage; 
--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Router, Route, hashHistory } from 'react-router';
+import createLogger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import LandingPage from './LandingPage.jsx';
 
 const App = () => (
-  <h1>Hello World!!!</h1>
+  <Router history={hashHistory}>
+    <Route path='/' component={LandingPage}/>
+  </Router>
 )
 
 export default App;
